440799bebdad6429dcace4a333c679ae808636a4
chiwadola <chiwadola@gmail.com>
2021-12-16 20:57:27 +0900

    jdbctemplate 추가, 로그인 화면 작업 중

--- src/main/java/anabada/semi/member/model/service/MemberService.java
+++ src/main/java/anabada/semi/member/model/service/MemberService.java
@@ -1,5 +1,15 @@
 package anabada.semi.member.model.service;
 
+import anabada.semi.member.model.vo.Member;
+
 public class MemberService {
+
+	public Member login(String memberId, String memberPw) throws Exception {
+		
+
+		
+		
+		return null;
+	}
 
 }
