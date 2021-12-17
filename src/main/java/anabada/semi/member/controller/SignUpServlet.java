85977dc48bd16e6bef381076e63635dbf9c4e5cf
chiwadola <chiwadola@gmail.com>
2021-12-17 16:40:52 +0900

    헤더의 로그인 회원가입 버튼 클릭시 로그인 회원가입 페이지로 이동

---
+++ src/main/java/anabada/semi/member/controller/SignUpServlet.java
@@ -0,0 +1,22 @@
+package anabada.semi.member.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/member/signUp")
+public class SignUpServlet extends HttpServlet{
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		String path = "/WEB-INF/views/member/signUpPage.jsp";
+		req.getRequestDispatcher(path).forward(req, resp);
+		
+	}
+	
+}
