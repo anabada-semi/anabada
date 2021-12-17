df231bc45470dad079476c9fbe4a8f4c2f4b6248
wooheesu <hsoo321@naver.com>
2021-12-17 17:40:01 +0900

    버튼에 글꼴 적용안되는것 수정 (header + footer.css에 button 글꼴 적용)

--- src/main/java/anabada/semi/item/model/service/ItemSelectService.java
+++ src/main/java/anabada/semi/item/model/service/ItemSelectService.java
@@ -2,10 +2,30 @@ package anabada.semi.item.model.service;
 
 import static anabada.semi.common.JDBCTemplate.*;
 
+import java.sql.Connection;
+import java.util.List;
+
 import anabada.semi.item.model.dao.ItemSelectDAO;
+import anabada.semi.item.model.vo.Item;
 
 public class ItemSelectService {
 
 	private ItemSelectDAO dao = new ItemSelectDAO();
+
+	
+	/** 카테고리 조회
+	 * @return categoryList
+	 * @throws Exception
+	 */
+	public List<Item> selectCategory() throws Exception{
+		
+		Connection conn = getConnection();
+		
+		List<Item> categoryList = dao.selectCategory(conn);
+		
+		close(conn);
+		
+		return categoryList;
+	}
 	
 }
