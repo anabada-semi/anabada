824b35d984872764e0c10c7e498aea46baed7677
DESKTOP-K5P3LA0\CHOYJ <dydwls140@naver.com>
2021-12-19 16:47:35 +0900

    jsp 스타일 css로 옮김

--- src/main/webapp/resources/js/myShop/myShop.js
+++ src/main/webapp/resources/js/myShop/myShop.js
@@ -1,3 +1,70 @@
+$(".btn").on("click", function(){
+		
+    $(".btn").removeClass("active");
+    $(this).addClass("active");
+
+    $("#changeDiv").empty();
+    
+    switch($(this).index()){
+        case 0: 
+            $.ajax({
+                url: "myShop/itemList",
+                success: function(r){
+                    $("#changeDiv").append(r);
+                },
+                error:function(req,status,er){
+                    console.log(req.responseText);
+                }
+            }); break;
+
+        case 1: 
+            $.ajax({
+                url: "myShop/wishList",
+                success: function(r){
+                    $("#changeDiv").append(r);
+                },
+                error:function(req,status,er){
+                    console.log(req.responseText);
+                }
+            }); break;
+
+        case 2: 
+            $.ajax({
+                url: "myShop/postScript",
+                success: function(r){
+                    $("#changeDiv").append(r);
+                },
+                error:function(req,status,er){
+                    console.log(req.responseText);
+                }
+            }); break;
+
+        case 3: 
+            $.ajax({
+                url: "myShop/buyList",
+                success: function(r){
+                    $("#changeDiv").html(r);
+                },
+                error:function(req,status,er){
+                    console.log(req.responseText);
+                }
+            }); break;
+
+        case 4: 
+            $.ajax({
+                url: "myShop/sellList",
+                success: function(r){
+                    $("#changeDiv").html(r);
+                },
+                error:function(req,status,er){
+                    console.log(req.responseText);
+                }
+            }); break;
+        default:
+    }
+});
+
+
 $(document).on("click",".shopNameBtn", function(){
     $("#save").empty();
     $("#save").append("<input type='text' id='userName'>");
@@ -21,16 +88,18 @@ $(document).on("click",".userNameBtn", function(){
 });
 
 $(document).on("click", "#myShopContent2", function(){
-
     
     const parent = $("#myShopContent1");
     parent.empty();
     parent.append("<textarea name='' id='contentTextarea' cols='30' rows='10' placeholder='소개글을 입력해 주세요.' style='resize: none; border:0.3px solid #ccc; width: 82%; height: 149px;'></textarea>");
     
     $("#contentTextarea").val( $("#myShopContent").text() );
+
+    const textLength = $("#contentTextarea").val().length;
+
     $("#myShopContent").text("");
     
-    parent.append("<button id='myShopContentBtn' style='float: right;'>확인</button>");
+    parent.append("<button id='myShopContentBtn' style='float: right;'>확인<br>" + textLength + "/120</button>");
 
 });
 
@@ -42,4 +111,16 @@ $(document).on("click", "#myShopContentBtn", function(){
     parent.append('<div id="myShopContent2" style="padding: 0px 10px 0px 10px; font-weight: lighter; border: 0.3px solid #ccc; font-size: 12px; display: inline-block;">소개글 수정</div>');
 
     $("#myShopContent").text(contentTextarea.val() );
+});
+
+$(document).on("input", "#contentTextarea", function(){
+    const textLength = $(this).val().length;
+    const textCountLimit = 120;
+    
+    $("#myShopContentBtn").html("확인<br>" + textLength + "/120");
+
+    if(textLength > textCountLimit){
+        $(this).val($(this).val().substr(0, textCountLimit));
+        $("#myShopContentBtn").html("확인<br>120/120");
+    }
 });
