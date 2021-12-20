f4b3cb1d32087e2325ee30da44e735b13deab81e
ducky <ducky46@naver.com>
2021-12-20 10:06:09 +0900

    item/insert, item/update 세팅

---
+++ src/main/java/anabada/semi/item/model/service/ItemService.java
@@ -0,0 +1,12 @@
+package anabada.semi.item.model.service;
+
+import static anabada.semi.common.JDBCTemplate.*;
+
+import anabada.semi.item.model.dao.ItemDAO;
+
+public class ItemService {
+	
+	private ItemDAO dao = new ItemDAO();
+	
+
+}
